66ed7e0addf70001afb9442a6e1a4e82e4c1f0a2
William Wilder <WilliamJohnWilder@outlook.com>
2021-05-13 06:23:12 -0700

    ADD: New Logo

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import algoLogo from "/src/assets/favicon.png";
+
+const Logo = () => (
+  <img src={algoLogo} alt="AlgoDS docs logo" style={{ width: 180 }} />
+);
+
+export default Logo;
