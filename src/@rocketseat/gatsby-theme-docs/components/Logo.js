f53ce1c0333454e64d931ead1713cd6a10e2dfc0
William Wilder <WilliamJohnWilder@outlook.com>
2021-05-14 07:29:40 -0700

    FIX: Logo

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 import algoLogo from "/src/assets/brandLogo.png";
 
 const Logo = () => (
-  <img src={algoLogo} alt="AlgoDS docs logo" style={{ width: 180 }} />
+  <img src={algoLogo} alt="AlgoDS docs logo" style={{ width: 150 }} />
 );
 
 export default Logo;
